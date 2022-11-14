51d19d064e3271f6b0a5b9ae32750457c426a439
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-14 12:55:01 +0530

    new commit

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -70,6 +70,11 @@ def publish_workbook(server, data):
     """
     Funcrion Description
     """
+    if data['is_workbook_new']:
+        publish_type = "CreateNew"
+    else:
+        publish_type = "Overwrite"
+
     project_id = get_project(server, data)
     wb_path = os.path.dirname(os.path.realpath(__file__)).rsplit(
         '/', 1)[0] + "/workbooks/" + data['file_path']
@@ -77,7 +82,7 @@ def publish_workbook(server, data):
     new_workbook = TSC.WorkbookItem(
         name=data['name'], project_id=project_id, show_tabs=data['show_tabs'])
     new_workbook = server.workbooks.publish(
-        new_workbook, wb_path, 'Overwrite', hidden_views = data['hidden_views'] if len(data['hidden_views']) > 0 else None )
+        new_workbook, wb_path, publish_type, hidden_views=data['hidden_views'] if len(data['hidden_views']) > 0 else None)
 
     print(
         f"\nSuccessfully published {data['file_path']} Workbook in {data['project_path']} project in {data['site_name']} site.")
@@ -209,7 +214,8 @@ def main(arguments):
                 if data['permissions']:
                     for permission_data in data['permissions']:
                         if permission_data['permission_template']:
-                            print("---------------------------------------------------------------------------------------------------------------------------------")
+                            print(
+                                "---------------------------------------------------------------------------------------------------------------------------------")
 
                             is_group = None
 
