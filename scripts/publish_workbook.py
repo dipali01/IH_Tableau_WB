4a78d501728faa317ea14ecedf35a7883509e536
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-14 12:47:17 +0530

    new commit

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -73,10 +73,11 @@ def publish_workbook(server, data):
     project_id = get_project(server, data)
     wb_path = os.path.dirname(os.path.realpath(__file__)).rsplit(
         '/', 1)[0] + "/workbooks/" + data['file_path']
+
     new_workbook = TSC.WorkbookItem(
         name=data['name'], project_id=project_id, show_tabs=data['show_tabs'])
     new_workbook = server.workbooks.publish(
-        new_workbook, wb_path, 'Overwrite', hidden_views=data['hidden_views'])
+        new_workbook, wb_path, 'Overwrite', hidden_views = data['hidden_views'] if len(data['hidden_views']) > 0 else None )
 
     print(
         f"\nSuccessfully published {data['file_path']} Workbook in {data['project_path']} project in {data['site_name']} site.")
