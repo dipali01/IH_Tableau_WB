4199673ce78e75e4e36a847805f9726b8347466a
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-17 18:54:22 +0530

    new commit

--- scripts/main.py
+++ scripts/main.py
@@ -23,6 +23,7 @@ def main(arguments):
             ds_id = get_ds_id(server, data['ds_name'])
 
             print("ds_id ::", ds_id)
+            
             dl_ds(server, ds_id[0])
 
             # publish_ds(server, data)
