c4aaa5505d4ca69a892ee377972e54be27821296
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-22 18:52:22 +0530

    new commit

--- scripts/service.py
+++ scripts/service.py
@@ -12,12 +12,14 @@ def temp_func(data, username, password, prod_username, prod_password):
     Funcrion Description
     """
     # Step: Sign In to the Tableau Server
+    if data['publish_wb_data']['server_name'] == "dev":
+        uname, pname, surl = username, password, data['dev_server_url']
+    elif data['publish_wb_data']['server_name'] == "prod":
+        uname, pname, surl = prod_username, prod_password, data['prod_server_url']
+
     server, auth_token, version = sign_in(
-        username if data['publish_wb_data']['server_name'] == "dev" else prod_username,
-        password if data['publish_wb_data']['server_name'] == "dev" else prod_password,
-        data['dev_server_url'] if data['publish_wb_data']['server_name'] == "dev" else data['prod_server_url'],
-        data['publish_wb_data']['site_name'],
-        data['publish_wb_data']['is_site_default']
+        uname, pname, surl, data['publish_wb_data'][
+            'site_name'], data['publish_wb_data']['is_site_default']
     )
 
     # Publish Workbook Part
@@ -93,17 +95,21 @@ def temp_func(data, username, password, prod_username, prod_password):
     try:
         if data['is_datasource_update']:
             # Step: Sign In to the Tableau Server
+            if data['datasource']['get_ds_data']['get_ds_server_name'] == "dev":
+                uname, pname, surl = username, password, data['dev_server_url']
+            elif data['datasource']['get_ds_data']['get_ds_server_name'] == "prod":
+                uname, pname, surl = prod_username, prod_password, data['prod_server_url']
+
             server, auth_token, version = sign_in(
-                username if data['datasource']['get_ds_data']['get_ds_server_name'] == "dev" else prod_username,
-                password if data['datasource']['get_ds_data']['get_ds_server_name'] == "dev" else prod_password,
-                data['dev_server_url'] if data['datasource']['get_ds_data']['get_ds_server_name'] == "dev" else data['prod_server_url'],
-                '' if data['datasource']['get_ds_data']['is_site_default'] else data['datasource']['get_ds_data']['get_ds_site_name'],
+                uname, pname, surl,
+                data['datasource']['get_ds_data']['get_ds_site_name'],
                 data['datasource']['get_ds_data']['is_site_default']
             )
 
             # Get datasource id from the name and project name
             ds_id = get_ds_id(
-                server, data['datasource']['ds_name'], data['datasource']['get_ds_data']['get_ds_project_name'])[0]
+                server, data['datasource']['ds_name'],
+                data['datasource']['get_ds_data']['get_ds_project_name'])[0]
 
             # Download datasource
             dl_ds_file_path = dl_ds(server, ds_id)
@@ -112,11 +118,14 @@ def temp_func(data, username, password, prod_username, prod_password):
             server.auth.sign_out()
 
             # Step: Sign In to the Tableau Server
+            if data['datasource']['publish_ds_data']['publish_ds_server_name'] == "dev":
+                uname, pname, surl = username, password, data['dev_server_url']
+            elif data['datasource']['publish_ds_data']['publish_ds_server_name'] == "prod":
+                uname, pname, surl = prod_username, prod_password, data['prod_server_url']
+
             server, auth_token, version = sign_in(
-                username if data['datasource']['publish_ds_data']['publish_ds_server_name'] == "dev" else prod_username,
-                password if data['datasource']['publish_ds_data']['publish_ds_server_name'] == "dev" else prod_password,
-                data['dev_server_url'] if data['datasource']['publish_ds_data']['publish_ds_server_name'] == "dev" else data['prod_server_url'],
-                '' if data['datasource']['publish_ds_data']['is_site_default'] else data['datasource']['publish_ds_data']['publish_ds_site_name'],
+                uname, pname, surl,
+                data['datasource']['publish_ds_data']['publish_ds_site_name'],
                 data['datasource']['publish_ds_data']['is_site_default']
             )
 
