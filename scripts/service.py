d340b5e302f3c86fa41a4d9dff3e32a09948da8c
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-23 11:40:15 +0530

    new commit

--- scripts/service.py
+++ scripts/service.py
@@ -139,6 +139,7 @@ def temp_func(data, username, password, prod_username, prod_password):
             # Step: Sign Out to the Tableau Server
             server.auth.sign_out()
     except Exception as tableu_exception:
-        logging.error(
-            "Something went wrong in datasource update.\n %s", tableu_exception)
-        exit(1)
+        exit(f"Something went wrong in datasource update.\n {tableu_exception}")
+        # logging.error(
+        #     "Something went wrong in datasource update.\n %s", tableu_exception)
+        # exit(1)
