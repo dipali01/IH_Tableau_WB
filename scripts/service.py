6934957a42fde002589b9c503b507be28c3a0933
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-23 15:36:26 +0530

    new commit

--- scripts/service.py
+++ scripts/service.py
@@ -67,8 +67,8 @@ def temp_func(data, username, password, prod_username, prod_password):
                         print(
                             f"\tPermission {permission_name} is set to {permission_mode} Successfully in {wb_id}\n")
                     else:
-                        print(' '.join(user_permissions))
                         for permission in user_permissions:
+                            print(permission.__dict__)
                             print(
                                 f"permission name: {permission.get('name')}, permission mode: {permission.get('mode')}")
 
