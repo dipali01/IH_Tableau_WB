59537d3f05c35c959af946b6b91db5bd1b7f00f3
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-24 19:37:01 +0530

    new commit

--- scripts/service.py
+++ scripts/service.py
@@ -1,7 +1,6 @@
 """
 Neccessory Module imports
 """
-import os
 import logging
 from publish import publish_wb, publish_ds
 from helpers import sign_in, get_group_id, get_user_id, get_ds_id, dl_ds, ds_refresh
