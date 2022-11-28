4df04d6880b7717b4c423c6e82cf9a14a6fd9235
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-28 12:28:46 +0530

    new commit

--- scripts/service.py
+++ scripts/service.py
@@ -168,5 +168,6 @@ def service_func(data, username, password, prod_username, prod_password, mpd):
     except Exception as tableu_exception:
         mpd[tmp]['_is_' + data['publish_wb_data']
                            ['wb_name'] + '_datasource_updated'] = False
+        print(mpd)
         logging.error(
             "Something went wrong in publish datasource.\n %s", tableu_exception)
