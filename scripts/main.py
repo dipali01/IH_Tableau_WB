a218c6a8715aa10549ea18ccc2d5d00b67e61dfc
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-28 14:48:32 +0530

    new commit

--- scripts/main.py
+++ scripts/main.py
@@ -45,7 +45,7 @@ def main(arguments):
         iter_split_end += num_proc
         jobs = []
 
-        print('\n')
+        print('_'*25)
         for i in mpd:
             for key, val in i.items():
                 if key == "wb_name":
